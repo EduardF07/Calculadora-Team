524cd9e42cf03cac931a6d84a7cfdaf3d85c65a3
hamza <hamza@hamza>
2026-04-15 12:46:42 +0200

    division

--- CalculadoraApp/src/Calculadora.java
+++ CalculadoraApp/src/Calculadora.java
@@ -1,10 +1,12 @@
 
 public class Calculadora {
+	
 	public double sumar(double a, double b) { return a + b; }
 	public double restar(double a, double b) { return a - b; }
+ 
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
-		System.out.println("--- CALCULADORA DEL JEFE A ---");
+		System.out.println("--- CALCULADORA DEL PROGRAMADOR B  ---");
 		System.out.println("Suma: " + calc.sumar(10, 5));
  }
 }
