03f2cd23cfd87faf063d482a050c37d2a05c3176
hamza <hamza@hamza>
2026-04-15 12:18:49 +0200

    division

--- CalculadoraApp/src/Calculadora.java
+++ CalculadoraApp/src/Calculadora.java
@@ -1,7 +1,10 @@
 
 public class Calculadora {
+	
 	public double sumar(double a, double b) { return a + b; }
 	public double restar(double a, double b) { return a - b; }
+	public double dividir(double a, double b) { return a / b; } 
+
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
 		System.out.println("--- CALCULADORA DEL JEFE A ---");
